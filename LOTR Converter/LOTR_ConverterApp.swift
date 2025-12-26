e5aee2469f4b450569e42ad27f6e6df38e25007e
MaheshB77 <mbansode7696@gmail.com>
2025-12-26 12:55:21 +0530

    Initial Commit

---
+++ LOTR Converter/LOTR_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_ConverterApp.swift
+//  LOTR Converter
+//
+//  Created by Mahesh Bansode on 26/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LOTR ConverterTests/LOTR_ConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_ConverterTests.swift
+//  LOTR ConverterTests
+//
+//  Created by Mahesh Bansode on 26/12/25.
+//
+
+import Testing
+@testable import LOTR_Converter
+
+struct LOTR_ConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
