e72c014607974dcbaf3b8f14208b572f7fa9940f
MaheshB77 <mbansode7696@gmail.com>
2025-12-26 19:26:10 +0530

    Initial commit

--- LOTR Converter/ContentView.swift
+++ LOTR Converter/ContentView.swift
@@ -8,14 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showInfoScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            // BG Image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+
+            VStack {
+                // Horse img
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100)
+
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+
+                HStack {
+
+                    // Left value
+                    VStack {
+                        HStack {
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 40)
+                            Text("Silver Piece")
+                                .foregroundStyle(.white)
+                                .font(.headline)
+                        }
+                        Text("Text Field")
+                    }
+
+                    // Equal sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+
+                    // Right value
+                    VStack {
+                        HStack {
+
+                            Text("Gold Piece")
+                                .foregroundStyle(.white)
+                                .font(.headline)
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 40)
+                        }
+                        Text("Text Field")
+                    }
+
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    Button {
+                        showInfoScreen.toggle()
+                        print("Showing info screen \(showInfoScreen)")
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundStyle(.white)
+                    }.padding(.trailing, 28)
+                }
+
+            }
         }
-        .padding()
     }
 }
 
